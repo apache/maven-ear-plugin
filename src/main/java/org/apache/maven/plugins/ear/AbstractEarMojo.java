a00bbe93688311f5270ef3df41971b53b8e4ec2b
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-06-09 23:40:39 +0200

    Improve documentation for version parameter
    fix #206

--- src/main/java/org/apache/maven/plugins/ear/AbstractEarMojo.java
+++ src/main/java/org/apache/maven/plugins/ear/AbstractEarMojo.java
@@ -57,7 +57,11 @@ public abstract class AbstractEarMojo extends AbstractMojo {
     public static final String UTF_8 = "UTF-8";
 
     /**
-     * The version of the application.xml to generate. Valid values are 1.3, 1.4, 5, 6, 7, 8, 9, 10 and 11.
+     * The schema version of generated <code>application.xml</code> descriptor.
+     * <p>
+     * Valid values are 1.3, 1.4, 5, 6, 7, 8, 9, 10 and 11.
+     * <p>
+     * See: <a href="https://jakarta.ee/xml/ns/jakartaee/">Jakarta EE XML Schemas</a>
      */
     @Parameter(defaultValue = "7")
     protected String version;
